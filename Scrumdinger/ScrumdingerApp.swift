466aa6a40db7230d53c9b9abfc426e1bd53623b2
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-07-22 18:32:50 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ishaan Vashishth  on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
